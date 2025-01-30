f6ab59ccbfb02a47c3bb92151f48da3e18d84e04
velumani-venkatesh <velumani_venkatesh@apple.com>
2025-01-30 15:57:45 +0530

    Account check again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h2>Venkatesh V</h2>
+      <h2>Venkatesh Velumani</h2>
     </div>
   );
 }
